ec58e4b2b329d281e67e2f1299c56b319ff29ccd
Marianna Bezler <marianna@famo.us>
2015-05-16 23:33:38 -0700

    Initial configuration changes

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,8 +2,8 @@
 
 // Responsive Nav
 var navigation = responsiveNav("#site-nav", { // Selector: The ID of the wrapper
-  animate: true, // Boolean: Use CSS3 transitions, true or false
-  transition: 200, // Integer: Speed of the transition, in milliseconds
+  animate: false, // Boolean: Use CSS3 transitions, true or false
+  transition: 0, // Integer: Speed of the transition, in milliseconds
   label: "<i class='fa fa-bars'></i> Menu", // String: Label for the navigation toggle
   insert: "before", // String: Insert the toggle before or after the navigation
   customToggle: "", // Selector: Specify the ID of a custom toggle
